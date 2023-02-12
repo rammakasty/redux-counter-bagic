d8cd41611f1d22da2fc72abfdbd064615313f33c
choi ha young <gkdudk15@naver.com>
2023-02-12 17:49:59 +0900

    feat: 오타수정

---
+++ src/redux/modules/counter.js
@@ -0,0 +1,12 @@
+const initialState = {
+    nombur: 0,
+};
+
+const counter = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default counter;
